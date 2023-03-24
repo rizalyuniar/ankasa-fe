c1f8ab796b8fa3aec2153b014dd4e38a7106cb8a
muhamad iqbal apredo <edo.vivoy21@gmail.com>
2023-03-24 20:00:18 +0700

    update slicing

--- src/views/flightDetail/index.jsx
+++ src/views/flightDetail/index.jsx
@@ -1,21 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import styles from './style.module.css';
-import Navbar from '../../components/navbar/index';
-import Footer from '../../components/footer/index';
-import garuda from '../../assets/airline.png';
-import icFlight from '../../assets/flight.svg';
-import ceklis from '../../assets/icCentang.svg';
-import bnr from '../../assets/pesawat.svg';
-import icWarning from '../../assets/warning.svg';
-import icDot from '../../assets/icDot.svg';
-import icDown from '../../assets/btnback.svg';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-import swal from 'sweetalert';
-import Swal from 'sweetalert2';
-import { useDispatch } from 'react-redux';
-import { createFlightBooking, getDetailFlight } from '../../redux/action/flightAction';
+import React, { useState, useEffect } from "react";
+import styles from "./style.module.css";
+import Navbar from "../../components/navbar/index";
+import Footer from "../../components/footer/index";
+import garuda from "../../assets/airline.png";
+import icFlight from "../../assets/flight.svg";
+import ceklis from "../../assets/icCentang.svg";
+import bnr from "../../assets/pesawat.svg";
+import icWarning from "../../assets/warning.svg";
+import icDot from "../../assets/icDot.svg";
+import icDown from "../../assets/btnback.svg";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import swal from "sweetalert";
+import Swal from "sweetalert2";
+import { useDispatch } from "react-redux";
+import {
+  createFlightBooking,
+  getDetailFlight,
+} from "../../redux/action/flightAction";
 
 const FlightDetail = () => {
   //get flight detail
@@ -23,26 +26,26 @@ const FlightDetail = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
 
-  const [fullname, setFullname] = useState('');
-  const [email, setEmail] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   useEffect(() => {
-    setFullname(localStorage.getItem('fullname'));
-    setEmail(localStorage.getItem('email'));
-    setPhoneNumber(localStorage.getItem('phone_number'));
+    setFullname(localStorage.getItem("fullname"));
+    setEmail(localStorage.getItem("email"));
+    setPhoneNumber(localStorage.getItem("phone_number"));
     dispatch(getDetailFlight(setData, id));
   }, [id, dispatch]);
 
   //booking
 
   const [formData, setFormData] = useState({
-    title: '',
-    fullname: '',
-    booking_name: '',
-    email: '',
-    phone_number: '',
-    nationality: '',
+    title: "",
+    fullname: "",
+    booking_name: "",
+    email: "",
+    phone_number: "",
+    nationality: "",
     insurance: false,
   });
 
@@ -60,7 +63,7 @@ const FlightDetail = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     dispatch(createFlightBooking(formData, id, token));
   };
 
@@ -87,29 +90,67 @@ const FlightDetail = () => {
                     <p className={styles.textLabelCP}>Fullname</p>
                     <div className="input-group mb-4">
                       {/* <input type="text" className={`form-control ${styles.inputStyle}`} aria-label="Username" placeholder="Full Name" aria-describedby="basic-addon1" value={formData.booking_name} /> */}
-                      <input type="text" className={`form-control ${styles.inputStyle}`} aria-label="Username" placeholder="Full Name" aria-describedby="basic-addon1" value={fullname} />
+                      <input
+                        type="text"
+                        className={`form-control ${styles.inputStyle}`}
+                        aria-label="Username"
+                        placeholder="Full Name"
+                        aria-describedby="basic-addon1"
+                        value={fullname}
+                      />
                     </div>
                     <p className={styles.textLabelCP}>Email</p>
                     <div className="input-group mb-4">
                       {/* <input type="email" className={`form-control ${styles.inputStyle}`} placeholder="Email" aria-label="Email" aria-describedby="basic-addon1" value={formData.email} /> */}
-                      <input type="email" className={`form-control ${styles.inputStyle}`} placeholder="Email" aria-label="Email" aria-describedby="basic-addon1" value={email} />
+                      <input
+                        type="email"
+                        className={`form-control ${styles.inputStyle}`}
+                        placeholder="Email"
+                        aria-label="Email"
+                        aria-describedby="basic-addon1"
+                        value={email}
+                      />
                     </div>
                     <p className={styles.textLabelCP}>Phone Number</p>
                     <div className="input-group mb-4">
                       {/* <input type="text" className={`form-control ${styles.inputStyle}`} placeholder="Phone" aria-label="Phone" aria-describedby="basic-addon1" value={formData.phone_number} /> */}
-                      <input type="text" className={`form-control ${styles.inputStyle}`} placeholder="Phone" aria-label="Phone" aria-describedby="basic-addon1" value={phoneNumber} />
+                      <input
+                        type="text"
+                        className={`form-control ${styles.inputStyle}`}
+                        placeholder="Phone"
+                        aria-label="Phone"
+                        aria-describedby="basic-addon1"
+                        value={phoneNumber}
+                      />
                     </div>
-                    <div className={`d-flex flex-row p-3 mt-3 ${styles.reminder}`}>
+                    <div
+                      className={`d-flex flex-row p-3 mt-3 ${styles.reminder}`}
+                    >
                       <img src={icWarning} alt="warning" />
-                      <div className="ms-3">Make sure the customer data is correct.</div>
+                      <div className="ms-3">
+                        Make sure the customer data is correct.
+                      </div>
                     </div>
                   </div>
                 </div>
                 <p className={`mt-5 ${styles.textTitle}`}>Passenger Details</p>
 
                 <div className={`p-4 ${styles.passengerDetail}`}>
-                  <div className={`d-flex flex-row p-3 mb-4 ${styles.reminderPassenger}`}>
-                    <div className={styles.textPassenger}>Passenger : 1 Adult</div>
+                  <div
+                    className={`d-flex flex-row p-3 mb-4 ${styles.reminderPassenger}`}
+                  >
+                    <div className={styles.textPassenger}>
+                      Passenger : 1 Adult
+                    </div>
+                    <div class="form-check form-switch ms-auto">
+                      <input
+                        class="form-check-input"
+                        type="checkbox"
+                        role="switch"
+                        id="flexSwitchCheckDefault"
+                        style={{width:'40px', height:'20px'}}
+                      />
+                    </div>
                   </div>
 
                   <div className="d-flex flex-column">
@@ -118,7 +159,13 @@ const FlightDetail = () => {
                         Title:
                       </label>
                       <div className="input-group mb-4">
-                        <select id="title" name="title" value={formData.title} onChange={handleInputChange} className={`form-control ${styles.inputStyle}`}>
+                        <select
+                          id="title"
+                          name="title"
+                          value={formData.title}
+                          onChange={handleInputChange}
+                          className={`form-control ${styles.inputStyle}`}
+                        >
                           <option value="">Choose a title</option>
                           <option value="1">Mr</option>
                           <option value="2">Ms</option>
@@ -143,12 +190,21 @@ const FlightDetail = () => {
                       />
                     </div>
                     <div>
-                      <label htmlFor="nationality" className={styles.textLabelCP}>
+                      <label
+                        htmlFor="nationality"
+                        className={styles.textLabelCP}
+                      >
                         Nationality:
                       </label>
 
                       <div className="input-group mb-4">
-                        <select id="nationality" name="nationality" value={formData.nationality} onChange={handleInputChange} className={`form-control ${styles.inputStyle}`}>
+                        <select
+                          id="nationality"
+                          name="nationality"
+                          value={formData.nationality}
+                          onChange={handleInputChange}
+                          className={`form-control ${styles.inputStyle}`}
+                        >
                           <option value="">Choose a nationality</option>
                           <option value="1">WNI</option>
                           <option value="2">WNA</option>
@@ -158,18 +214,32 @@ const FlightDetail = () => {
                   </div>
                 </div>
 
-                <label className={`mt-5 ${styles.textTitle}`}>Passenger Details</label>
+                <label className={`mt-5 ${styles.textTitle}`}>
+                  Passenger Details
+                </label>
                 <div className={`p-4 ${styles.passengerDetail}`}>
                   <div className="d-flex flex-row">
-                    <input class="form-check-input mt-1 me-3" type="checkbox" value="" aria-label="Checkbox for following text input" id="insurance" name="insurance" checked={formData.insurance} onChange={handleCheckboxChange} />
-                    <p className={styles.textInsurance}>Travel Insurance</p>
+                    <input
+                      class="form-check-input mt-2 me-3 ms-2"
+                      type="checkbox"
+                      value=""
+                      aria-label="Checkbox for following text input"
+                      id="insurance"
+                      name="insurance"
+                      checked={formData.insurance}
+                      onChange={handleCheckboxChange}
+                    />
+                    <p style={{marginLeft:'30px', marginTop:'2px'}} className={styles.textInsurance}>Travel Insurance</p>
                     {/* <p className={`ms-auto ${styles.textPriceinsurance}`}>
                       $ {data.price}
                     </p> */}
                     {/* <p className={`mt-1 ${styles.textPax}`}>/pax</p> */}
                   </div>
                   <hr />
-                  <p className={styles.textCompensation}>Get travel compensation up to $ 10.000,00</p>
+                  <p className={styles.textCompensation}>
+                    Get travel compensation up to Rp.{" "}
+                    <b className="text-danger">10.000,00</b>
+                  </p>
                 </div>
                 <div class="d-grid gap-2 col-6 mt-5 mx-auto">
                   <button class={`btn ${styles.cstmButton}`} type="submit">
@@ -182,7 +252,7 @@ const FlightDetail = () => {
               {data.map((row) => (
                 <>
                   <div className="d-flex flex-row flex-wrap">
-                    <img src={row.image} alt="garuda" style={{ height: '80px', objectFit: 'cover' }} />
+                    <img width={170} height={80} src={row.image} alt="garuda" />
                     <p className={`ms-3 ${styles.textGaruda}`}>{row.airline}</p>
                   </div>
                   <div className="d-flex flex-row mt-4">
@@ -213,7 +283,9 @@ const FlightDetail = () => {
                   <hr />
                   <div className="d-flex flex-row mt-3 flex-wrap">
                     <div className={styles.textTotal}>Total Payment</div>
-                    <div className={`ms-auto me-3 ${styles.textPrice}`}>Rp. {row.price}</div>
+                    <div className={`ms-auto me-3 ${styles.textPrice}`}>
+                      Rp. {row.price}
+                    </div>
                     <img src={icDown} alt="down" />
                   </div>
                 </>
